135a16cbc548a89bf253ecd8c2fde3442b61c0b4
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2026-04-26 11:22:13 -0300

    Tasks models created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('P', 'Pendente'),
+        ('A', 'Em Andamento'),
+        ('C', 'Concluído'),
+    ]
+    title = models.CharField(max_Length=120)
+    description = models.TextField()
+    status = models.CharField(max_Length=1, choices=STATUS_CHOICES, default='P')
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField()
+
+    def __str__(self):
+        return self.title
